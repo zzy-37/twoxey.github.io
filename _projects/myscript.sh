3efc04ca3a876a9ee9f6e2e66305026cdfa3ea6c
twoxey <id18_zzy@tongji.edu.cn>
2021-06-19 03:58:41 +0800

    added image processing script

--- _projects/myscript.sh
+++ _projects/myscript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-list=$(find -name "*.png")
+list=$(find -iname "*.png")
 for val in $list; do
-read dir name <<< `echo $val | awk -F/ '{OFS="/"; n=$NF; $NF=""; print $0" "n}'`
-echo $dir $name
+    read dir name <<< `echo $val | awk -F/ '{OFS="/"; n=$NF; $NF=""; print $0" "n}'`
+    #echo $dir $name
+    rname=`echo $name|awk -F. '{print $1}'`
+    # echo ${dir}scl-${rname}.jpg
+    convert $val ${dir}${rname}.jpg
 done
