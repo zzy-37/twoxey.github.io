7b7758d70ad3fef817c3f7247f5c5c5c1fd7258e
twoxey <id18_zzy@tongji.edu.cn>
2021-07-02 01:55:16 +0800

    update

---
+++ assets/index.js
@@ -0,0 +1,63 @@
+let lock = false;
+
+function topFunction() {
+    let x = document.getElementsByClassName("tab");  // 找到元素
+    for (let i = 0; i < x.length; i++) {
+        x[i].scrollTo(0, 0);
+    }
+    return false;
+}
+
+function select(t) {
+    if (!lock) {
+        t.classList.toggle('select');
+        topFunction();
+    }
+}
+
+function hideIntro() {
+    const intro = document.querySelector(".intro");
+    intro.classList.add('out');
+    setTimeout(() => {
+        intro.style.display = "none"
+    }, 1500);
+
+    const out = document.querySelectorAll(".out");
+    out.forEach((ele, i) => {
+        setTimeout(() => {
+            ele.classList.toggle('out')
+        }, 200 * i + 1500)
+    });
+}
+
+function tagin() {
+    const ins = document.querySelectorAll(".in");
+    const imgs = document.querySelectorAll(".imgin");
+
+    ins.forEach((ele, i) => {
+        setTimeout(() => {
+            ele.classList.toggle('in')
+        }, 200 * i)
+    });
+
+    imgs.forEach(i => {
+        setTimeout(() => {
+            i.classList.toggle('imgin');
+        }, 2000)
+    });
+}
+
+const options = {
+    root: null,
+    threshold: 0.9
+}
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            tagin();
+            observer.unobserve(entry.target)
+        }
+    })
+}, options)
+
+observer.observe(document.querySelector(".tabs"));
